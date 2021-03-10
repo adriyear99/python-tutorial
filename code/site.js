d8dbc4da311b79128e92a558e54dcd24d1676d11
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-03-10 19:40:45 -0300

    versão teste

---
+++ code/site.js
@@ -0,0 +1,6 @@
+function iniciar() { 
+  document.getElementById("demo").innerHTML = "O teste será iniciado...";
+  document.getElementById("demo").style.fontSize = "25px";
+  document.getElementById("demo").style.color = "red";
+  document.getElementById("botao").innerHTML = "Carregando...";
+}
