278a0b0af14dad5bc63e4d4a8eb5094ceef3cd53
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-03-22 01:42:00 -0300

    versão teste

--- code/main.js
+++ code/main.js
@@ -1,11 +1,13 @@
-function iniciar() { 
-  document.getElementById("demo").innerHTML = "O teste será iniciado...";
-  document.getElementById("demo").style.fontSize = "25px";
-  document.getElementById("demo").style.color = "red";
-  document.getElementById("botao").innerHTML = "Carregando...";
-}
+function start() {
+	let valor = confirm('Parabéns por finalizar o tutorial. Agora seu conhecimento será testado.');
 
-function start(){
-	alert("Parabéns por finalizar o tutorial. Agora seu conhecimento será testado.")
-	window.location.href = "teste.html";
+	if (valor){
+		document.getElementById("demo").innerHTML = "O teste será iniciado...";
+		document.getElementById("demo").style.fontSize = "25px";
+		document.getElementById("demo").style.color = "red";
+		document.getElementById("botao").innerHTML = "Carregando...";
+		setTimeout(function change(){
+			window.location.href = "teste.html";
+		},2000);
+	}
 }
