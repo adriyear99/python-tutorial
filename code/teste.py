f52f11c4dfb7d6279c20d794c3b9c2b2408840a8
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-03-25 06:19:59 -0300

    versão teste

--- code/teste.py
+++ code/teste.py
@@ -1,12 +1,22 @@
 # Teste
 
+''' Questão 1 '''
+
+print("Uma linguagem de alto nível possui uma sintaxe voltada para entendimento humano")
+print("de forma que o usuário possa entender com mais facilidade")
+print("-------------------------------------------------------------------------------")
+
+
 
 ''' Questão 2 '''
 
 var1 = 5
 var2 = "8"
-res = var1 + var2
-print(res)
+# res = var1 + var2
+# print(res)
+print("ERRO DE TIPAGEM")
+print("-------------------------------------------------------------------------------")
+
 
 
 ''' Questão 3 '''
@@ -15,3 +25,70 @@ var1 = 10
 var2 = 2
 res = var1 % var2
 print(res)
+print("-------------------------------------------------------------------------------")
+
+
+
+''' Questão 4 '''
+
+var1 = 10
+var2 = 2
+var3 = 3
+
+if (var1 % var3 == 1) and (var1 ** var2 == 100):
+    print("As duas condições são verdadeiras")
+elif (var1 % var3 == 1) and not(var1 ** var2 == 100):
+    print("Apenas a primeira condição é verdadeira")
+elif not(var1 % var3 == 1) and (var1 ** var2 == 100):
+    print("Apenas a segunda condição é verdadeira")
+else:
+    print("As duas condições são falsas")
+
+print("-------------------------------------------------------------------------------")
+
+
+
+''' Questão 5 '''
+
+
+
+
+
+''' Questão 6 '''
+
+inicio = 4
+final = 16
+intervalo = 2
+
+for x in range(inicio,final,intervalo):
+    print(x)
+
+
+
+''' Questão 7 '''
+
+var = 5
+
+while var < 10:
+    print(var)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
