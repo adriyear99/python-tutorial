03ee33e21c1405acd774d3a45be2e7dd106b73d2
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-03-28 02:46:00 -0300

    versão teste

---
+++ code/python_tutorial.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE teste;
+USE teste;
+CREATE TABLE resultados(
+	nome	varchar(60) NOT NULL,
+    q1		varchar(1) NOT NULL,
+    q2		char(1) NOT NULL,
+    q3		char(1) NOT NULL,
+    q4		char(1) NOT NULL,
+    q5		char(1) NOT NULL,
+    q6		char(1) NOT NULL,
+    q7		char(1) NOT NULL,
+    q8		char(1) NOT NULL,
+    q9		char(1) NOT NULL,
+    q10		char(1) NOT NULL
+);
+
+DROP TABLE resultados;
+
+SELECT * FROM resultados;
