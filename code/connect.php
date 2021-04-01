207587a09cebd1732500b3e537b33b7210150198
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-04-01 14:15:51 -0300

    versão teste

--- code/connect.php
+++ code/connect.php
@@ -1,21 +1,21 @@
 <?php
 
-	$host = 'localhost';
+	$host = '127.0.0.1';
 	$bd = 'teste';
 	$usuariobd = 'root';
 	$senhabd = '';
 
-	$nome = $_POST['nome']
-	$q1 = $_POST['q1']
-	$q2 = $_POST['q2']
-	$q3 = $_POST['q3']
-	$q4 = $_POST['q4']
-	$q5 = $_POST['q5']
-	$q6 = $_POST['q6']
-	$q7 = $_POST['q7']
-	$q8 = $_POST['q8']
-	$q9 = $_POST['q9']
-	$q10 = $_POST['q10']
+	$nome = $_POST['nome'] ?? "";
+	$q1 = $_POST['q1'] ?? "";
+	$q2 = $_POST['q2'] ?? "";
+	$q3 = $_POST['q3'] ?? "";
+	$q4 = $_POST['q4'] ?? "";
+	$q5 = $_POST['q5'] ?? "";
+	$q6 = $_POST['q6'] ?? "";
+	$q7 = $_POST['q7'] ?? "";
+	$q8 = $_POST['q8'] ?? "";
+	$q9 = $_POST['q9'] ?? "";
+	$q10 = $_POST['q10'] ?? "";
 
 	$conexao = mysqli_connect($host,$userbd,$senhabd);
 
