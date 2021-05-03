13847a97fe9a74b2d48135566148e4387ee0f54c
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2021-05-03 07:00:10 -0300

    atualização

--- code/connect.php
+++ code/connect.php
@@ -75,8 +75,8 @@
 		?>
 
 		<nav class="centro">
-			<button type="button" id="botao3" class="botao bt3" onclick="result()">Ver resultados</button>
-			<button type="button" id="botao4" class="botao bt4" onclick="answer()">Ver gabarito</button>
+			<button type="button" id="botao3" class="botao tipo1" onclick="result()">Ver resultados</button>
+			<button type="button" id="botao4" class="botao tipo1" onclick="answer()">Ver gabarito</button>
 		</nav>
 
 		<br><br>
